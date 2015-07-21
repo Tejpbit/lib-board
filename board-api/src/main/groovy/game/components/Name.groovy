e927506a1e7fdb244afea82e9246837b669fd1e9
André Samuelsson <andresamuelsson94@gmail.com>
2015-07-21 16:30:50 +0200

    removed components and passive system

--- board-api/src/main/groovy/game/components/Name.groovy
+++
@@ -1,8 +0,0 @@
-package game.components
-
-/**
- * Created by tejp on 16/07/15.
- */
-class Name implements Component{
-    String name
-}
--- board-api/src/main/groovy/game/components/Text.groovy
+++
@@ -1,8 +0,0 @@
-package game.components
-
-/**
- * Created by tejp on 16/07/15.
- */
-class Text implements Component{
-    String text
-}
